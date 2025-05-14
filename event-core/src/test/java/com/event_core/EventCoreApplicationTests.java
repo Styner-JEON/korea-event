f6a93eb066be56b1d79fb383aec23210100685ec
KANG-IL JEON <styner@KANG-ILs-MacBook-Pro.local>
2025-05-14 11:31:26 +0900

    first commit

---
+++ event-core/src/test/java/com/event_core/EventCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
